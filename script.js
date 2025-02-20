7126c12e2ae2af592c17443907e1e73699a6f4b3
blednyikaklanaya <hootro34@gmail.com>
2025-02-20 22:08:16 +0200

    feat|fix: create show elements step to step during scroll site. new animation in CSS. temporary debug code line.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 let buttonArray = document.getElementsByClassName("switcher-button");
+let switcherImg = document.getElementsByClassName("switcher-img")[0];
+let reccalCircleImg = document.getElementsByClassName("reccal-green-circle")[0];
 
-console.log(buttonArray);
+console.log(reccalCircleImg); // debug
+
+function observerChecker (nameElement, nameClass, loaded) {
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add(nameClass);
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {threshold: loaded});
+    
+    observer.observe(nameElement);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    observerChecker(switcherImg, "visible", 0.1);
+    observerChecker(reccalCircleImg, "visible-cirle", 0.1);
+
+    let allElements = Array.from(document.getElementsByTagName("*")).filter(el => !["SCRIPT", "STYLE", "META", "LINK"].includes(el.tagName));
+    console.log(allElements); 
+
+    allElements.forEach(element => observerChecker(element, "unlock", 0.1));
+});
 
 
 function handleClickButton(button) {
@@ -8,12 +35,14 @@ function handleClickButton(button) {
         button.classList.remove("active-button");
     }
 
-    button.classList.toggle("active-button");
+    setTimeout(() => {
+        button.classList.toggle("active-button");
+    }, 50)
 }
 
 for (let i = 0; i < buttonArray.length; i++) {
     buttonArray[i].addEventListener("click", () => {
         handleClickButton(buttonArray[i]);
-        console.log("hello!");
+        console.log("click!");// debug
     })
 }
